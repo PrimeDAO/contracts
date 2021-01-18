bf094a2c12355ea8244c00b3d3adf56df2fe1bc5
arsenyjin <kinspotting@protonmail.com>
2021-01-18 14:55:12 +0100

    fix remove test gas setup

--- truffle-config.js
+++ truffle-config.js
@@ -33,9 +33,7 @@ module.exports = {
       development: {
           host: 'localhost',
           port: 8545,
-          network_id: 1234,
-          gas: 4700000,
-          gasPrice: 20 * 1000000000
+          network_id: 1234
       },
       coverage: {
           host: '127.0.0.1',
