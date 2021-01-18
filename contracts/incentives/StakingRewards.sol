ef712a6af962384a33e991b7489c7435b1380045
mfahampshire <>
2021-01-18 19:05:42 +0100

    fixed compiler error

--- contracts/incentives/StakingRewards.sol
+++ contracts/incentives/StakingRewards.sol
@@ -114,8 +114,8 @@ contract StakingRewards is IRewardDistributionRecipient, ReentrancyGuard {
          // This keeps the reward rate in the right range, preventing overflows due to
          // very high values of rewardRate in the earned and rewardsPerToken functions;
          // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
-         // uint balance = rewardToken.balanceOf(address(this));
-         // require(rewardRate <= balance.div(DURATION), "StakingRewards: Provided reward too high");
+         uint balance = IERC20(rewardToken).balanceOf(address(this));
+         require(rewardRate <= balance.div(DURATION), "StakingRewards: Provided reward too high");
 
          lastUpdateTime = block.timestamp;
          periodFinish = block.timestamp.add(DURATION);
