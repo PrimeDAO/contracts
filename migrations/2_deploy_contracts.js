97b724c052cbe16a10501f8cb60c992ce1c5ccda
mfahampshire <mfahampshire@gmail.com>
2021-04-07 13:50:47 +0200

    added multiple test seeds

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -23,19 +23,19 @@ module.exports = async function (deployer, network) {
 
     if (network === 'mainnet') {
 
-        // await deployer.deploy(StakingRewards);
-        // await deployer.deploy(PriceOracle);
-        // await deployer.deploy(LockingToken4Reputation);
-        // await deployer.deploy(BalancerProxy);
-        // await deployer.deploy(VestingFactory);
-        // await deployer.deploy(RepRedeemer);
-        //
-        // contracts.mainnet.StakingRewards = StakingRewards.address;
-        // contracts.mainnet.PriceOracle = PriceOracle.address;
-        // contracts.mainnet.LockingToken4Reputation = LockingToken4Reputation.address;
-        // contracts.mainnet.BalancerProxy = BalancerProxy.address;
-        // contracts.mainnet.VestingFactory = VestingFactory.address;
-        // contracts.mainnet.RepRedeemer = RepRedeemer.address;
+        await deployer.deploy(StakingRewards);
+        await deployer.deploy(PriceOracle);
+        await deployer.deploy(LockingToken4Reputation);
+        await deployer.deploy(BalancerProxy);
+        await deployer.deploy(VestingFactory);
+        await deployer.deploy(RepRedeemer);
+
+        contracts.mainnet.StakingRewards = StakingRewards.address;
+        contracts.mainnet.PriceOracle = PriceOracle.address;
+        contracts.mainnet.LockingToken4Reputation = LockingToken4Reputation.address;
+        contracts.mainnet.BalancerProxy = BalancerProxy.address;
+        contracts.mainnet.VestingFactory = VestingFactory.address;
+        contracts.mainnet.RepRedeemer = RepRedeemer.address;
 
         // overwrite contranctAddresses.json
         fs.writeFile('./contractAddresses.json', JSON.stringify(contracts), (err) => {
@@ -60,7 +60,7 @@ module.exports = async function (deployer, network) {
         contracts.kovan.VestingFactory = VestingFactory.address;
         contracts.kovan.RepRedeemer = RepRedeemer.address;
         contracts.kovan.SeedFactory = SeedFactory.address;
-        contracts.kovan.Seed = Seed.address;
+        contracts.kovan.SeedParent = Seed.address;
 
         // overwrite contranctAddresses.json
         fs.writeFile('./contractAddresses.json', JSON.stringify(contracts), (err) => {
