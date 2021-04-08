98cfba77da6c60c6e98a9e0dc25c0d39b60a7969
mfahampshire <mfahampshire@gmail.com>
2021-04-08 13:36:44 +0200

    updated seed deploy script for new starttimes

--- scripts/kovan/deploySeeds.js
+++ scripts/kovan/deploySeeds.js
@@ -15,32 +15,33 @@ module.exports = async function(callback) {
         let seedFactory = await SeedFactory.at(contracts.kovan.SeedFactory);
         let seedToken = await PrimeToken.at(contracts.kovan.PrimeToken);
 
-        let now = 1617796022;
+        let now = Date.now();
+        let oneDay = 86400;
         let twoDays = 172800;
         let fourDays = 345600;
         let sevenDays = 604800;
         let nineDays = 777600;
 
-        let admin              = process.env.ACCOUNT;
+        let admin           = process.env.ACCOUNT;
         let fundingToken    = [ contracts.kovan.WETH, contracts.kovan.DAI, contracts.kovan.WETH, contracts.kovan.DAI, contracts.kovan.WETH ];
         let cap             = [ toWei('100'), toWei('100'), toWei('150'), toWei('300'), toWei('200') ];
         let price           = [ toWei('0.01'), toWei('0.04'), toWei('0.02'), toWei('0.03'), toWei('0.01') ];
         let successMinimum  = [ toWei('20'), toWei('20'), toWei('70'), toWei('200'), toWei('50') ];
-        // let startTime       = now; // add future dates
-        let endTime         = [ now + sevenDays, now + sevenDays, now + sevenDays, now + nineDays, now + twoDays ];
+        let startTime       = [ now + oneDay, now + twoDays, now + fourDays, now + sevenDays, now + nineDays ];
+        let endTime         = [ startTime[0] + sevenDays,  startTime[1] + sevenDays, startTime[2] + sevenDays, startTime[3] + nineDays, startTime[4] + twoDays ];
         let vestingDuration = [ 365, 400, 365, 185, 365 ]; // 1 year
         let vestingCliff    = [ 90, 95, 80, 60, 120 ]; // 3 months
         let isWhitelisted   = [ false, false, true, true, false ];
         let fee             = [ 2, 2, 1, 5, 2 ];
 
-        await seedToken.approve(SeedFactory.address, cap[0]);
+        await seedToken.approve(seedFactory.address, cap[0]);
         let seedAddress1 = await seedFactory.deploySeed(
             admin,
             contracts.kovan.PrimeToken,
             fundingToken[0],
             [successMinimum[0],cap[0]],
             price[0],
-            startTime,
+            startTime[0],
             endTime[0],
             vestingDuration[0],
             vestingCliff[0],
@@ -49,14 +50,14 @@ module.exports = async function(callback) {
         );
         console.log("deployed seed at " + seedAddress1.logs[0].args.newSeed);
 
-        await seedToken.approve(SeedFactory.address, cap[1]);
+        await seedToken.approve(seedFactory.address, cap[1]);
         let seedAddress2 = await seedFactory.deploySeed(
             admin,
             contracts.kovan.PrimeToken,
             fundingToken[1],
             [successMinimum[1],cap[1]],
             price[1],
-            startTime,
+            startTime[1],
             endTime[1],
             vestingDuration[1],
             vestingCliff[1],
@@ -65,14 +66,14 @@ module.exports = async function(callback) {
         );
         console.log("deployed seed at " + seedAddress2.logs[0].args.newSeed);
 
-        await seedToken.approve(SeedFactory.address, cap[2]);
+        await seedToken.approve(seedFactory.address, cap[2]);
         let seedAddress3 = await seedFactory.deploySeed(
             admin,
             contracts.kovan.PrimeToken,
             fundingToken[2],
             [successMinimum[2],cap[2]],
             price[2],
-            startTime,
+            startTime[2],
             endTime[2],
             vestingDuration[2],
             vestingCliff[2],
@@ -81,14 +82,14 @@ module.exports = async function(callback) {
         );
         console.log("deployed seed at " + seedAddress3.logs[0].args.newSeed);
 
-        await seedToken.approve(SeedFactory.address, cap[3]);
+        await seedToken.approve(seedFactory.address, cap[3]);
         let seedAddress4 = await seedFactory.deploySeed(
             admin,
             contracts.kovan.PrimeToken,
             fundingToken[3],
             [successMinimum[3],cap[3]],
             price[3],
-            startTime,
+            startTime[3],
             endTime[3],
             vestingDuration[3],
             vestingCliff[3],
@@ -97,14 +98,14 @@ module.exports = async function(callback) {
         );
         console.log("deployed seed at " + seedAddress4.logs[0].args.newSeed);
 
-        await seedToken.approve(SeedFactory.address, cap[4]);
+        await seedToken.approve(seedFactory.address, cap[4]);
         let seedAddress5 = await seedFactory.deploySeed(
             admin,
             contracts.kovan.PrimeToken,
             fundingToken[4],
             [successMinimum[4],cap[4]],
             price[4],
-            startTime,
+            startTime[4],
             endTime[4],
             vestingDuration[4],
             vestingCliff[4],
