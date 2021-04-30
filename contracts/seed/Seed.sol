d1e212a4532fee0019bf3288f9be9c3e31465ba9
Konstantin <BritikovKI@Yandex.ru>
2021-04-30 18:42:39 +0300

    linter fix

--- contracts/seed/Seed.sol
+++ contracts/seed/Seed.sol
@@ -159,7 +159,8 @@ contract Seed {
     */
     function buy(uint256 _seedAmount) public protected checked {
         uint256 fundingAmount = _seedAmount.mul(price).div(PCT_BASE);
-        require((fundingToken.balanceOf(address(this)).add(fundingAmount)) <= cap, "Seed: amount exceeds contract sale cap");
+        require((fundingToken.balanceOf(address(this)).add(fundingAmount)) <= cap,
+            "Seed: amount exceeds contract sale cap");
         require( seedToken.balanceOf(address(this)) >=
         (fundingToken.balanceOf(address(this)).mul(price).div(PCT_BASE)).add(_seedAmount),
             "Seed: seed distribution exceeded");
