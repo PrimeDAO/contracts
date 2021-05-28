37b5ad175483fe204223c89a835055fbb8eb0390
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-28 15:57:34 +0530

    made clear check

--- contracts/seed/SeedFactory.sol
+++ contracts/seed/SeedFactory.sol
@@ -43,7 +43,7 @@ contract SeedFactory is CloneFactory, Ownable {
      * @param _masterCopy The address of the new Seed basis.
      */
     function changeMasterCopy(Seed _masterCopy) public onlyOwner {
-        require(address(_masterCopy) != address(0), "SeedFactory: mastercopy cannot be zero address");
+        require(_masterCopy != Seed(0), "SeedFactory: mastercopy cannot be zero address");
         masterCopy = _masterCopy;
     }
 
