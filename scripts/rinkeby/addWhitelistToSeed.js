f26a85e72a8fede269ab4aac84616b104c68a655
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-07-06 17:41:42 +0530

    added correct logging for feedback

--- scripts/rinkeby/addWhitelistToSeed.js
+++ scripts/rinkeby/addWhitelistToSeed.js
@@ -1,13 +1,12 @@
 /*global web3, artifacts*/
-require('dotenv').config();
+require("dotenv").config();
 const Seed = artifacts.require("Seed");
-const seedDetails = require('../../seedDetails.json');
-const axios = require('axios');
+const seedDetails = require("../../seedDetails.json");
+const axios = require("axios");
 
-const toAscii = (str1) =>
-{
-    let hex  = str1.toString();
-    let str = '';
+const toAscii = (str1) => {
+    let hex = str1.toString();
+    let str = "";
     for (let n = 0; n < hex.length; n += 2) {
         str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
     }
@@ -15,22 +14,28 @@ const toAscii = (str1) =>
 };
 
 const fetchWhitelist = async (url) => {
-    const whitelists = (await axios.get(url)).data.split(',');
-    return whitelists.map((account)=>{
-        return account.replace(/\n/g, '');
+    const whitelists = (await axios.get(url)).data.split(",");
+    return whitelists.map((account) => {
+        return account.replace(/\n/g, "");
     });
 };
 
 module.exports = async () => {
-    try {
-        const seed = await Seed.at("0xE2BE04B1E834CD737E210B67d961e18Ba9feA03C");
-        const response = await axios.get(`https://ipfs.io/ipfs/QmbhX5YEh9umusWL1mxp7G5NJQ6UzKPjDzzTsWF7BaqsfW`);
-        const whitelists = await fetchWhitelist(JSON.parse(response.data).seedDetails.whitelist);
-        console.log("address to be whitelisted:- ", whitelists);
-        seed.whitelistBatch(whitelists).then(
-            () => console.log("Succesfull!")
-        );
-    } catch (error) {
-        console.log(error);
-    }
+    const seed = await Seed.at("0xE2BE04B1E834CD737E210B67d961e18Ba9feA03C");
+    const response = await axios.get(`https://ipfs.io/ipfs/QmbhX5YEh9umusWL1mxp7G5NJQ6UzKPjDzzTsWF7BaqsfW`);
+    const whitelists = await fetchWhitelist(JSON.parse(response.data).seedDetails.whitelist);
+    console.log("address to be whitelisted:- ", whitelists);
+    seed.whitelistBatch(whitelists)
+        .then(() => {
+            whitelists.forEach(async (address) => {
+                const status = await seed.whitelisted(address);
+                if (status) {
+                    console.log(`${address} is whitelisted`);
+                    return;
+                }
+                console.log(`${address} is not whitelisted`);
+            });
+            console.log("Succesfull!");
+        })
+        .catch((error) => console.log(error));
 };
